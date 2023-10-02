cd07633e9e5f2b7a1e9d98493720a6f8357f39b8
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-02 03:10:25 -0700

    Display main menu options & list of all Tokis

--- src/com/nmathias/Tokimon/game/TokimonList.java
+++ src/com/nmathias/Tokimon/game/TokimonList.java
@@ -15,4 +15,53 @@ public class TokimonList {
     public static List<Tokimon> getTokimonList() {
         return tokiList;
     }
+
+    /*
+    Display a main menu for the user to select the desired operation to
+    be performed on the Tokimon
+     */
+    public static void displayMainMenu() {
+        for (int i = 0; i < 13; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        System.out.print("* Main Menu *\n");
+        for (int i = 0; i < 13; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        System.out.print("1. List Tokimons\n");
+        System.out.print("2. Add a new Tokimon\n");
+        System.out.print("3. Remove a Tokimon\n");
+        System.out.print("4. Change Tokimon strength\n");
+        System.out.print("5. DEBUG: Dump objects (toString)\n");
+        System.out.print("6. Exit\n");
+    }
+
+    /*
+    Display the entire list of Tokimons so that the user can keep track
+    and find a particular Tokimon much more efficiently
+     */
+    public static void displayAllTokis() {
+        for (int i = 0; i < 21; i++) {
+            System.out.print("*");
+        }
+
+        System.out.print("\n");
+        System.out.print("* List of Tokimons: *\n");
+
+        for (int i = 0; i < 21; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+
+        for (int i = 0; i < tokiList.size(); i++) {
+            System.out.print((i + 1) + ". Toki " + tokiList.get(i).getName() + ", "
+                    + tokiList.get(i).getHeight() + "m height, "
+                    + tokiList.get(i).getWeight() + "g weight, "
+                    + tokiList.get(i).getType() + " ability, "
+                    + tokiList.get(i).getStrength() + " strength\n");
+        }
+    }
+
 }
