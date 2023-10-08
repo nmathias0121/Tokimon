4e2e0ec2aa320462d2e27fc939ebd3e21fa8f4b7
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-08 14:51:25 -0700

    add file to test tokimon list

---
+++ src/com/nmathias/Tokimon/test/TokimonListTest.java
@@ -0,0 +1,32 @@
+package src.com.nmathias.Tokimon.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokimonListTest {
+
+    @Test
+    void getTokimonList() {
+    }
+
+    @Test
+    void displayMainMenu() {
+    }
+
+    @Test
+    void displayAllTokis() {
+    }
+
+    @Test
+    void addNewToki() {
+    }
+
+    @Test
+    void deleteToki() {
+    }
+
+    @Test
+    void alterToki() {
+    }
+}
