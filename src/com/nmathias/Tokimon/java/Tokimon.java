47589d1a0d638827d95db09367303ce2468f0f84
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-07 18:42:16 -0700

    remove set functions

--- src/com/nmathias/Tokimon/java/Tokimon.java
+++ src/com/nmathias/Tokimon/java/Tokimon.java
@@ -26,32 +26,16 @@ public class Tokimon {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public double getHeight() {
         return height;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
     public double getWeight() {
         return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
     }
 
     public double getStrength() {
